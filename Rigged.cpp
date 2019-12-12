43ee800922593046b3798490ed53d5af3d418042
JeremyC57 <jcasadaelliott@mail.csuchico.edu>
2019-12-11 16:15:26 -0800

    added Files for 'Rigged' Class

---
+++ Rigged.cpp
@@ -0,0 +1,13 @@
+#include "Rigged.h"
+
+using namespace std;
+
+void Rigged::enterName(string nameEntry)
+{
+        name = nameEntry;
+}
+
+string Rigged::winner()
+{
+        return name;
+}
---
+++ Rigged.h
@@ -0,0 +1,18 @@
+#ifndef _RIGGED_H_
+#define _RIGGED_H_
+#include <string>
+
+
+using namespace std;
+class Rigged
+{
+        private: string name;
+                 
+        
+        public:void enterName(string);
+               string winner();
+};
+
+
+
+#endif
